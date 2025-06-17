520698d53eccb2f584718032f85d4fc0a045bf40
ari <ariludkovski@gmail.com>
2025-06-16 18:54:06 -0700

    changed the regex again

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ app.command("/block-assign", async ({ command, ack, respond }) => {
 
   const schedule = loadSchedule();
   const assignments = {};
-  const roleRegex = /(\w+)=<@([A-Z0-9]+)(?:\|[^>]+)?>/g;
+  const roleRegex = /(\w+)=\s*<@([A-Z0-9]+)(?:\|[^>]+)?>/g;
   let match;
 
   while ((match = roleRegex.exec(text)) !== null) {
